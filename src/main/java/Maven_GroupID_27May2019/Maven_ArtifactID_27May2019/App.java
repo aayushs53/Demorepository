78007768c4d320ebfe4cf6360ba1a11ff377777a
Aayush Sharma <aayushs53@gmail.com>
2020-07-15 21:01:00 +0100

    First git commit for my selenium code

---
+++ src/main/java/Maven_GroupID_27May2019/Maven_ArtifactID_27May2019/App.java
@@ -0,0 +1,8 @@
+package Maven_GroupID_27May2019.Maven_ArtifactID_27May2019;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
